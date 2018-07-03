2782ebed9ec2b80992b24fd16aa092c7e1371db2
Robert Nye <robertanye@gmail.com>
2018-07-03 14:33:30 -0500

    Update the watch app so it will run

--- wear/src/main/java/com/dexdrip/stephenblack/nightwatch/NWPreferences.java
+++ wear/src/main/java/com/dexdrip/stephenblack/nightwatch/NWPreferences.java
@@ -2,14 +2,16 @@ package com.dexdrip.stephenblack.nightwatch;
 
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.preference.PreferenceActivity;
 
-public class NWPreferences extends PreferenceFragment {
+public class NWPreferences extends PreferenceActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
 
     }
 
